b46e5c2eebdbdc16e3ba2aea10d7ca6fb8a8702f
douglasproferreira <douglas.ferreira2@estudante.ifms.edu.br>
2019-05-25 19:32:10 -0400

    Primeira Fase do Script.js

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,66 +1,74 @@
 var alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var c
-var acento = ["ã", "á", "à", "â", "é", "è", "ê", "í", "î", "ì", "ó", "ô", "ò", "õ", "ú", "ù", "û", "ç"]
-var sAcento = ["a", "a", "a", "a", "e", "e", "e", "i", "i", "i", "o", "o", "o", "o", "u", "u", "u", "c"]
+var acento = ["ã", "á", "à", "â", "é", "è", "ê", "í", "î", "ì", "ó", "ô", "ò", "õ", "ú", "ù", "û", "ç"];
+var sAcento = ["a", "a", "a", "a", "e", "e", "e", "i", "i", "i", "o", "o", "o", "o", "u", "u", "u", "c"];
+var chave = []
 
-function capturarElemento(ref) {
+capturarElemento = (ref) => {
     const elemento = document.querySelector(ref);
     return elemento;
 }
 
-const criptografar = capturarElemento('form');
-criptografar.addEventListener('submit', function (event) {
-    console.log(criptografar)
-    verificarChave();
-    event.preventDefault();
-}, false)
-
-function verificarChave() {
-    let chave = capturarElemento('.chave');
-    c = chave.value
-    if (c > 26 || c <= 0) {
-        alert("Chave Inválida - Informe uma chave entre 1 e 26");
-        inner.HTML();
-    } else {
-        retirarAcento()
-    }
+verificarChave = () => {
+    let chave = capturarElemento('.chave').value.toLowerCase();
+    console.log(chave);
+    if (!chave.length) {
+        return alert('Chave não Inserida');
+    };
 }
 
-function retirarAcento() {
-    let aux = "", a = "";
-    let texto = capturarElemento('.texto').value.toLowerCase();
-    console.log(texto);
+preencherChave = () => {
+    let ch = retirarAcento(capturarElemento('.chave').value.toLowerCase());
+    console.log(ch);
+    for (let k = 0; k < ch.length; k++) {
+        for (let l = 0; l < alfabeto.length; l++) {
+            if (alfabeto[l] === ch[k]) {
+                chave[k] = l
+            }
+        }
+    }
+    console.log(chave)
+    return chave
+}
 
-    for (let i = 0; i < texto.length; i++) {
-        if (texto[i] != " ") {
+retirarAcento = (text) => {
+    let aux = "", a = "", texto = 0;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] != " ") {
             for (let j = 0; j < acento.length; j++) {
-                if (acento[j] == texto[i]) {
+                if (text[i] == acento[j]) {
                     aux += sAcento[j]
-                } else {
-                    a = texto[i]
+                    texto = 1
+                } else if (text[i] != acento[j]) {
+                    a = text[i]
                 }
             }
-            aux += a
-        } else if (texto[i] == " ") {
+            if (texto != 1) {
+                aux += a
+            }
+            texto = 0
+        } else if (text[i] == " ") {
             aux += " "
         }
     }
     console.log(aux);
-    criptografarTexto(aux);
+    return aux;
 }
 
-function criptografarTexto(aux) {
-    let ch = parseInt(c);
-    console.log(ch);
+criptografar = () => {
+    verificarChave();
+    let text = retirarAcento(capturarElemento('.texto').value.toLowerCase());
     let res = "", resultado = "";
-    console.log(aux)
-    console.log(aux.length)
-    for (let i = 0; i < aux.length; i++) {
-        if (aux[i] != " ") {
+    let c = preencherChave();
+    let anchor = 0;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] != " ") {
             for (let j = 0; j < alfabeto.length; j++) {
-                if (aux[i] == alfabeto[j]) {
-                    let pos = ((j + ch) % 26);
-                    console.log(pos);
+                if (text[i] == alfabeto[j]) {
+                    if (anchor > c.length - 1) {
+                        anchor = 0;
+                    }
+                    let pos = ((j + c[anchor]) % 26);
+                    anchor += 1;
                     res = alfabeto[pos];
                     resultado += res;
                 }
@@ -71,11 +79,40 @@ function criptografarTexto(aux) {
         }
     }
     console.log(resultado);
-    
     mostrarnaTela(resultado);
+    event.preventDefault();
 }
 
-function mostrarnaTela(resultado) {
+descriptografar = () => {
+    verificarChave();
+    let text = retirarAcento(capturarElemento('.texto').value.toLowerCase());
+    let res = "", resultado = "";
+    let c = preencherChave();
+    let anchor = 0;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] != " ") {
+            for (let j = 0; j < alfabeto.length; j++) {
+                if (text[i] == alfabeto[j]) {
+                    if (anchor > c.length - 1) {
+                        anchor = 0;
+                    }
+                    let pos = ((j - c[anchor]) % 26);
+                    anchor += 1;
+                    res = alfabeto[pos];
+                    resultado += res;
+                }
+            }
+        } else {
+            res = " ";
+            resultado += res;
+        }
+    }
+    console.log(resultado);
+    mostrarnaTela(resultado);
+    event.preventDefault();
+}
+
+mostrarnaTela = (resultado) => {
     let p = document.createElement('p')
     let conteudo = document.createTextNode(resultado)
     p.appendChild(conteudo)
